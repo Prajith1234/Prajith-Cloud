cc190b0ce90575864e7226612bcb70bae9dc7707
prajith1234 <challapkumar07@gmail.com>
2022-06-14 12:28:27 +0530

    firstcommit

---
+++ src/main/java/com/proper/CloudProper/CloudProperApplication.java
@@ -0,0 +1,13 @@
+package com.proper.CloudProper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudProperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proper/CloudProper/CloudProperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proper.CloudProper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
